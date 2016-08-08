56381e523c7c6069d68ac0087fc8fe2ae6501c15
Koziolek <bjkuczynski@gmail.com>
2016-08-08 20:59:15 +0200

    Remove loops by streams

--- src/main/java/com/splurth/SymbolGenerator.java
+++ src/main/java/com/splurth/SymbolGenerator.java
@@ -1,10 +1,14 @@
 package com.splurth;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Stream.concat;
 
 /**
  * Created by BKuczynski on 2016-08-08.
@@ -12,22 +16,43 @@ import java.util.stream.Stream;
 class SymbolGenerator {
 
 	public Collection<Symbol> generateAllValidSymbols(ChemicalElement element) {
-		return getSymbolStrings(element).collect(Collectors.toList());
+		return getSymbolStream(element).collect(Collectors.toList());
 	}
 
 	public Collection<Symbol> generateDistinctValidSymbols(ChemicalElement element) {
-		return getSymbolStrings(element).distinct().collect(Collectors.toList());
+		return getSymbolStream(element).distinct().collect(Collectors.toList());
 	}
 
-	private Stream<Symbol> getSymbolStrings(ChemicalElement element) {
+	private Stream<Symbol> getSymbolStream(ChemicalElement element) {
 		String name = element.getName().toLowerCase();
-		List<String> validSymbols = new LinkedList<>();
-		char[] chars = name.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			for (int j = i + 1; j < chars.length; j++) {
-				validSymbols.add(chars[i] + "" + chars[j]);
-			}
-		}
-		return validSymbols.stream().map(Symbol::new);
+		List<String> chars = Arrays.asList(name.split(""));
+
+		return generateStream(chars);
+	}
+
+	private Stream<Symbol> generateStream(List<String> chars) {
+		String head = head(chars);
+		List<String> tail = tail(chars);
+
+		Function<String, String> headTailConcat = s -> head + s;
+
+		if (tail.size() == 1)
+			return tail.stream().map(s -> head + s).map(Symbol::new);
+
+
+		return concat(
+				tail.stream().map(headTailConcat).map(Symbol::new),
+				generateStream(tail)
+		);
+	}
+
+
+	private <T> T head(List<T> list) {
+		return list.get(0);
+	}
+
+	private <T> List<T> tail(List<T> list) {
+		if (list.size() == 0) return Collections.emptyList();
+		return list.subList(1, list.size());
 	}
 }
