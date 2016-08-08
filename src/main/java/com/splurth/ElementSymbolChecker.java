1cbd9d7f6144097b26c588736574cd2f1313b18f
Koziolek <bjkuczynski@gmail.com>
2016-08-08 20:00:16 +0200

    REfactoring of checker

--- src/main/java/com/splurth/ElementSymbolChecker.java
+++ src/main/java/com/splurth/ElementSymbolChecker.java
@@ -1,24 +1,13 @@
 package com.splurth;
 
-import java.util.function.Predicate;
-
 /**
  * Created by BKuczynski on 2016-08-08.
  */
 public class ElementSymbolChecker {
 
-    public boolean validate(ChemicalElement element) {
-        Symbol symbol = element.getSymbol();
-        String elementName = element.getName().toLowerCase();
+	private SymbolGenerator symbolGenerator = new SymbolGenerator();
 
-        String regExp = ".*" + symbol.getFirst().toLowerCase() + ".*" + symbol.getSecond().toLowerCase() + ".*";
-
-        Predicate<Symbol> symbolLength = s -> s.getSymbol().length() == 2;
-        Predicate<Symbol> firstLetter = s -> elementName.contains(s.getFirst().toLowerCase());
-        Predicate<Symbol> secondLetter = s -> elementName.contains(s.getSecond().toLowerCase());
-        Predicate<Symbol> letterOrder = s -> elementName.matches(regExp);
-
-
-        return symbolLength.and(firstLetter).and(secondLetter).and(letterOrder).test(symbol);
-    }
+	public boolean validate(ChemicalElement element) {
+		return symbolGenerator.generateDistinctValidSymbols(element).contains(element.getSymbol());
+	}
 }
--- src/main/java/com/splurth/SymbolGenerator.java
+++ src/main/java/com/splurth/SymbolGenerator.java
@@ -4,31 +4,30 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by BKuczynski on 2016-08-08.
  */
-public class SymbolGenerator {
+class SymbolGenerator {
 
-    public Collection<Symbol> generateAllValidSymbols(ChemicalElement element){
-        List<String> validSymbols = getSymbolStrings(element);
-        return validSymbols.stream().map(Symbol::new).collect(Collectors.toList());
-    }
+	public Collection<Symbol> generateAllValidSymbols(ChemicalElement element) {
+		return getSymbolStrings(element).collect(Collectors.toList());
+	}
 
-    public Collection<Symbol> generateDistinctValidSymbols(ChemicalElement element){
-        List<String> validSymbols = getSymbolStrings(element);
-        return validSymbols.stream().map(Symbol::new).distinct().collect(Collectors.toList());
-    }
+	public Collection<Symbol> generateDistinctValidSymbols(ChemicalElement element) {
+		return getSymbolStrings(element).distinct().collect(Collectors.toList());
+	}
 
-    private List<String> getSymbolStrings(ChemicalElement element) {
-        String name = element.getName().toLowerCase();
-        List<String> validSymbols = new LinkedList<>();
-        char[] chars = name.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            for (int j = i + 1; j < chars.length; j++) {
-                validSymbols.add(chars[i] + "" + chars[j]);
-            }
-        }
-        return validSymbols;
-    }
+	private Stream<Symbol> getSymbolStrings(ChemicalElement element) {
+		String name = element.getName().toLowerCase();
+		List<String> validSymbols = new LinkedList<>();
+		char[] chars = name.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = i + 1; j < chars.length; j++) {
+				validSymbols.add(chars[i] + "" + chars[j]);
+			}
+		}
+		return validSymbols.stream().map(Symbol::new);
+	}
 }
